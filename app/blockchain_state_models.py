543f7efe30ef7e131aafdf57add1175235ff7f35
komazovaleksandr8-alt <komazovaleksandr8@gmail.com>
2026-08-22 13:41:53 +0300

    Update blockchain_state_models.py

--- app/blockchain_state_models.py
+++ app/blockchain_state_models.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import Column, DateTime, Integer
+from sqlalchemy import Column, DateTime, Integer, String
 
 from app.database import Base
 
