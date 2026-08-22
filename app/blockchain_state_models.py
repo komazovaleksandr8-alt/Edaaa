d06b31d5225c9b389ee1139bf4804c02c4112620
komazovaleksandr8-alt <komazovaleksandr8@gmail.com>
2026-08-22 13:41:06 +0300

    Create blockchain_state_models.py

---
+++ app/blockchain_state_models.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, DateTime, Integer
+
+from app.database import Base
+
+
+class BlockchainState(Base):
+    __tablename__ = "blockchain_state"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+
+    network = Column(
+        String(50),
+        nullable=False,
+        unique=True,
+    )
+
+    last_scanned_block = Column(
+        Integer,
+        nullable=False,
+        default=0,
+    )
+
+    updated_at = Column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
