3629195961b46e7f6ad1c984f5dc9152c3c18ea0
komazovaleksandr8-alt <komazovaleksandr8@gmail.com>
2026-08-22 03:13:51 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,12 +2,20 @@ import os
 
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 
 class Settings:
-    APP_NAME: str = os.getenv("APP_NAME", "Edaaa Wallet")
-    APP_VERSION: str = os.getenv("APP_VERSION", "0.1.0")
+    APP_NAME: str = os.getenv(
+        "APP_NAME",
+        "Edaaa Wallet",
+    )
+
+    APP_VERSION: str = os.getenv(
+        "APP_VERSION",
+        "0.1.0",
+    )
 
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL",
@@ -17,6 +25,16 @@ class Settings:
     JWT_SECRET: str = os.getenv(
         "JWT_SECRET",
         "CHANGE_THIS_SECRET_IN_PRODUCTION",
+    )
+
+    ETH_RPC_URL: str = os.getenv(
+        "ETH_RPC_URL",
+        "",
+    )
+
+    ETH_NETWORK: str = os.getenv(
+        "ETH_NETWORK",
+        "sepolia",
     )
 
 
